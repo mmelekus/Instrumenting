d28fabc81e49b50b283a614de4634c65e42a0179
MartinMZen <mmelekus@msn.com>
2023-07-26 01:47:15 -0500

    Fixed configuration bind bug

--- Instrumenting/Program.cs
+++ Instrumenting/Program.cs
@@ -14,8 +14,7 @@ builder.SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 IConfigurationRoot configuration = builder.Build();
 TraceSwitch ts = new(displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
-var config = configuration.GetSection("PacktSwitch");
-config.Bind(ts);
+configuration.GetSection("PacktSwitch").Bind(ts);
 Trace.WriteLineIf(ts.TraceError, "Trace error");
 Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
 Trace.WriteLineIf(ts.TraceInfo, "Trace information");
